de759a43a12f59cf14cd594c7e294b4a67369a00
Sergio Ribeiro <svbro@iscte-iul.pt>
2018-11-15 16:01:27 +0000

    Update to the facebook api key

--- Users/user.user-PC/eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.projects/src/pt/iscte/esi/projeto/form/models/FacebookMain.java
+++ Users/user.user-PC/eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.projects/src/pt/iscte/esi/projeto/form/models/FacebookMain.java
@@ -11,46 +11,15 @@ import com.restfb.types.User;
 
 //restfb
 public class FacebookMain {
-	
+	public static String Token="EAAD0JjeESBwBAL8OQ9yVoX5tGOqpgQiWhMyoyOYmtka1QWCVAlxxRzi97QFeUyAxOOb7mnpvwYiId2n6lr58iyqZBinEckQvj4ZBVKLHCfGuF61r1OdQMBidrpC2MNgLmCc2AupdDRGIvjJCMVembyTgm9FdCDX0sp1bld1w5VNLk5zZCKQHp19Gfg5aSXna1qFwZCDzkbnwKuP4KZCwG";
 	/**
 	 * Facebook API Main
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/* 
-		 * Facebook API Tutorials in Java # 1 | Setup Development Environment 
-		 * https://www.youtube.com/watch?v=m14hYs1T3FA&index=1&list=PLYPFxrXyK0BwiXNe09hTPjFqYbsWv8gxb
-		 */
-		/* 
-		 * Facebook API Tutorials in Java # 2 | Get User Access Token
-		 * https://www.youtube.com/watch?v=GwbO_PdwK_4&index=2&list=PLYPFxrXyK0BwiXNe09hTPjFqYbsWv8gxb
-		 */
-		String accessToken2 = "EAAD0JjeESBwBAHMZAmdkKgkZCnDiJxu1LLnZAONofCxZBlXZBMPnEc1L8TOZA6VjgvW5j7L7wxlAzN5N3UR818hnnO3tzlFn5kDHz4zN7PwIRrl7x6VKdyHfDlPAR7t7IMzDWINbGXo76fHh3xgsMZBHhF0ZCgTbsmGqSal9UwcbRgFpZA7y9VG71Yn9BIQ2EZArwivpUpLrGDOZAWenEEu6wKF";
-		FacebookClient fbClient2 = new DefaultFacebookClient(accessToken2);
-		User me2 = fbClient2.fetchObject("me", User.class);
-		System.out.println("Facebook:");
-		System.out.println("Id: " + me2.getId());
-		System.out.println("Name: " + me2.getName());
-
-		/* 
-		 * Facebook API Tutorials in Java # 4 | Create Your Own Fb APP & Extend User Access Token  
-		 * https://www.youtube.com/watch?v=qFZazZ1JXsM&list=PLYPFxrXyK0BwiXNe09hTPjFqYbsWv8gxb&index=5
-		 */
-		String accessToken4 = "EAAD0JjeESBwBAHMZAmdkKgkZCnDiJxu1LLnZAONofCxZBlXZBMPnEc1L8TOZA6VjgvW5j7L7wxlAzN5N3UR818hnnO3tzlFn5kDHz4zN7PwIRrl7x6VKdyHfDlPAR7t7IMzDWINbGXo76fHh3xgsMZBHhF0ZCgTbsmGqSal9UwcbRgFpZA7y9VG71Yn9BIQ2EZArwivpUpLrGDOZAWenEEu6wKF";
 		
-		FacebookClient fbClient4 = new DefaultFacebookClient(accessToken4);
-		AccessToken extendedAccessToken4 = fbClient4.obtainExtendedAccessToken("268444977350684","75a7638dfa1a64929a408b3955681d0a");
-		
-		System.out.println("ExtendedAccessToken: "+extendedAccessToken4.getAccessToken());
-		System.out.println("Expires: " + extendedAccessToken4.getExpires());
-
-		/* 
-		 * Facebook API Tutorials in Java # 5 | Get User Timeline Posts
-		 * https://www.youtube.com/watch?v=wiFif4gOdFE&index=6&list=PLYPFxrXyK0BwiXNe09hTPjFqYbsWv8gxb
-		*/ 
-		String accessToken5 ;//= "EAAGZBEccjciEBAJ37ZAIbHKiL1Mo1HHex2pQTcs41dq8azfBvFGgt4eGgKBq12kSssOof51FKO0niKu7AaVKs3dy8W1ilqp4xcjFD1F9mmjJpVyeDnZAffUXRfh7zXL06BuSwQtfHMJbmJ079qCnkT844brHx966cz73JZBZBFy2Bv1rWu7T1rQddZCVpxywZCO6lDxoWDk2gZDZD";
-		accessToken5 = "EAAD0JjeESBwBAHMZAmdkKgkZCnDiJxu1LLnZAONofCxZBlXZBMPnEc1L8TOZA6VjgvW5j7L7wxlAzN5N3UR818hnnO3tzlFn5kDHz4zN7PwIRrl7x6VKdyHfDlPAR7t7IMzDWINbGXo76fHh3xgsMZBHhF0ZCgTbsmGqSal9UwcbRgFpZA7y9VG71Yn9BIQ2EZArwivpUpLrGDOZAWenEEu6wKF";	
-		FacebookClient fbClient5 = new DefaultFacebookClient(accessToken5);
+		String accessToken=Token;
+		FacebookClient fbClient5 = new DefaultFacebookClient(accessToken);
 
 		Connection<Post> result = fbClient5.fetchConnection("me/feed",Post.class,  Parameter.with("type", "post"));
 		
