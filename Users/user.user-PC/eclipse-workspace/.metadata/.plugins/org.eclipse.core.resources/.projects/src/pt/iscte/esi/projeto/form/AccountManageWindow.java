b83d85b4114e17d8f34a6fde6c9d721841ec29df
josefsantos <jfnfs@iscte-iul.pt>
2018-11-30 21:04:13 +0000

    AccountManagerWindow changes

--- Users/user.user-PC/eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.projects/src/pt/iscte/esi/projeto/form/AccountManageWindow.java
+++ Users/user.user-PC/eclipse-workspace/.metadata/.plugins/org.eclipse.core.resources/.projects/src/pt/iscte/esi/projeto/form/AccountManageWindow.java
@@ -35,15 +35,22 @@ public class AccountManageWindow extends JFrame {
 	private JPanel contentPane;
 
 	private JTextField textField;
-	private JTextField textField_2;
+	private JTextField extendedAccessTokenFace1;
+	private JTextField extendedAccessTokenFace2;
 	private JTextField textField_4;
+	private JTextField mailMail;
+	private JTextField mail;
 	private JTextField textField_6;
-	private JTextField textField_1;
+	private JTextField accessTokenFace;
+	private JTextField authConsumKeyTwit;
 	private JTextField textField_3;
-	private JTextField textField_8;
+	private JTextField authAccessTokenSecretTwit;
+	private JTextField passMail;
 	private JTextField textField_9;
+	private JTextField authAccessTokenTwit;
 	private JTextField textField_10;
-	private JTextField textField_11;
+	private JTextField authConsumSecretTwit;
+	private JTextField usernameMail;
 
 	private XMLFileEditor editor = new XMLFileEditor();
 
@@ -74,7 +81,7 @@ public class AccountManageWindow extends JFrame {
 		 * Adds image to the window
 		 */
 		JLabel newLabel = new JLabel("");
-		newLabel.setBounds(87, 105, 119, 421);
+		newLabel.setBounds(45, 84, 119, 413);
 		ImageIcon image = new ImageIcon(AccountManageWindow.class.getResource("/pt/iscte/esi/projeto/form/images/LogosContas.png"));
 		Image img = image.getImage().getScaledInstance(newLabel.getWidth(), newLabel.getHeight(), Image.SCALE_SMOOTH);
 		newLabel.setIcon(new ImageIcon(img));
@@ -96,84 +103,66 @@ public class AccountManageWindow extends JFrame {
 		JLabel lblNewLabel = new JLabel(
 				"Adiciona os c\u00F3digos de acesso relativos a cada conta e recebe informa\u00E7\u00E3o acad\u00E9mica proveniente de cada canal.");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblNewLabel.setBounds(158, 49, 553, 14);
+		lblNewLabel.setBounds(158, 49, 516, 14);
 		contentPane.add(lblNewLabel);
 
-		/*
-		 * Adding the different "frame components" to the window
-		 */
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Desactivar mensagens deste canal");
-		rdbtnNewRadioButton.setBackground(Color.WHITE);
-		rdbtnNewRadioButton.setBounds(491, 171, 241, 23);
-		contentPane.add(rdbtnNewRadioButton);
+		accessTokenFace = new JTextField();
+		accessTokenFace.setColumns(10);
+		accessTokenFace.setBounds(377, 105, 366, 20);
+		contentPane.add(accessTokenFace);
 
-		JRadioButton rdbtnDesactivarMensagensDeste = new JRadioButton("Desactivar mensagens deste canal");
-		rdbtnDesactivarMensagensDeste.setBackground(Color.WHITE);
-		rdbtnDesactivarMensagensDeste.setBounds(491, 360, 225, 23);
-		contentPane.add(rdbtnDesactivarMensagensDeste);
+		textField = new JTextField();
 
-		JRadioButton radioButton = new JRadioButton("Desactivar mensagens deste canal");
-		radioButton.setBackground(Color.WHITE);
-		radioButton.setBounds(491, 485, 225, 23);
-		contentPane.add(radioButton);
+		extendedAccessTokenFace1 = new JTextField();
+		extendedAccessTokenFace1.setBounds(377, 140, 121, 20);
+		contentPane.add(extendedAccessTokenFace1);
+		extendedAccessTokenFace1.setColumns(10);
+
+		extendedAccessTokenFace2 = new JTextField();
+		extendedAccessTokenFace2.setColumns(10);
+		extendedAccessTokenFace2.setBounds(507, 140, 236, 20);
+		contentPane.add(extendedAccessTokenFace2);
+
+
+		authConsumSecretTwit = new JTextField();
+		authConsumSecretTwit.setColumns(10);
+		authConsumSecretTwit.setBounds(376, 240, 366, 20);
+		contentPane.add(authConsumSecretTwit);
+
+		mailMail = new JTextField();
+		mailMail.setBounds(377, 427, 366, 20);
+		contentPane.add(mailMail);
 
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
 		textField_6.setBounds(377, 105, 366, 20);
 		contentPane.add(textField_6);
 
-		textField = new JTextField();
-
-		textField = new JTextField();
-		textField.setBounds(377, 140, 121, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(507, 140, 236, 20);
-		contentPane.add(textField_2);
-
-
-		textField_10 = new JTextField();
-		textField_10.setColumns(10);
-		textField_10.setBounds(376, 240, 366, 20);
-		contentPane.add(textField_10);
-
-		textField_4 = new JTextField();
-		textField_4.setBounds(377, 427, 366, 20);
-		contentPane.add(textField_4);
-
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(377, 105, 366, 20);
-		contentPane.add(textField_6);
-
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(377, 270, 366, 20);
-		contentPane.add(textField_1);
+		authConsumKeyTwit = new JTextField();
+		authConsumKeyTwit.setColumns(10);
+		authConsumKeyTwit.setBounds(377, 270, 366, 20);
+		contentPane.add(authConsumKeyTwit);
 
 		textField_9 = new JTextField();
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(376, 333, 366, 20);
-		contentPane.add(textField_3);
+		authAccessTokenSecretTwit = new JTextField();
+		authAccessTokenSecretTwit.setColumns(10);
+		authAccessTokenSecretTwit.setBounds(376, 333, 366, 20);
+		contentPane.add(authAccessTokenSecretTwit);
 
-		textField_9 = new JTextField();
-		textField_9.setColumns(10);
-		textField_9.setBounds(376, 301, 366, 20);
-		contentPane.add(textField_9);
+		authAccessTokenTwit = new JTextField();
+		authAccessTokenTwit.setColumns(10);
+		authAccessTokenTwit.setBounds(376, 301, 366, 20);
+		contentPane.add(authAccessTokenTwit);
 
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
 		textField_3.setBounds(376, 333, 366, 20);
 		contentPane.add(textField_3);
 
-		textField_11 = new JTextField();
-		textField_11.setColumns(10);
-		textField_11.setBounds(377, 397, 366, 20);
-		contentPane.add(textField_11);
+		usernameMail = new JTextField();
+		usernameMail.setColumns(10);
+		usernameMail.setBounds(377, 397, 366, 20);
+		contentPane.add(usernameMail);
 
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
@@ -185,11 +174,11 @@ public class AccountManageWindow extends JFrame {
 		textField_10.setBounds(376, 240, 366, 20);
 		contentPane.add(textField_10);
 
-		textField_8 = new JTextField();
-		textField_8.setBounds(377, 458, 251, 20);
-		textField_8.setColumns(10);
-		textField_8.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		contentPane.add(textField_8);
+		passMail = new JTextField();
+		passMail.setBounds(377, 458, 251, 20);
+		passMail.setColumns(10);
+		passMail.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(passMail);
 
 		JLabel lblEmail = DefaultComponentFactory.getInstance().createLabel("Access token:");
 		lblEmail.setBounds(285, 105, 119, 14);
@@ -200,19 +189,19 @@ public class AccountManageWindow extends JFrame {
 		contentPane.add(lblNewJgoodiesLabel);
 
 		JLabel lblNewJgoodiesLabel_00 = DefaultComponentFactory.getInstance().createLabel("AuthConsumerSecret:");
-		lblNewJgoodiesLabel_00.setBounds(243, 240, 127, 14);
+		lblNewJgoodiesLabel_00.setBounds(243, 240, 123, 14);
 		contentPane.add(lblNewJgoodiesLabel_00);
 
 		JLabel lblNewJgoodiesLabel_0 = DefaultComponentFactory.getInstance().createLabel("AuthConsumerKey:");
-		lblNewJgoodiesLabel_0.setBounds(257, 270, 124, 14);
+		lblNewJgoodiesLabel_0.setBounds(257, 270, 110, 14);
 		contentPane.add(lblNewJgoodiesLabel_0);
 
 		JLabel lblNewJgoodiesLabel_2 = DefaultComponentFactory.getInstance().createLabel("Auth. access token:");
-		lblNewJgoodiesLabel_2.setBounds(254, 300, 124, 14);
+		lblNewJgoodiesLabel_2.setBounds(254, 300, 112, 14);
 		contentPane.add(lblNewJgoodiesLabel_2);
 
 		JLabel lblUsername = DefaultComponentFactory.getInstance().createLabel("Auth access \r\ntoken secret:");
-		lblUsername.setBounds(216, 335, 172, 14);
+		lblUsername.setBounds(228, 336, 138, 14);
 		contentPane.add(lblUsername);
 
 		JLabel lblUsernameMail = DefaultComponentFactory.getInstance().createLabel("Username: ");
@@ -220,11 +209,11 @@ public class AccountManageWindow extends JFrame {
 		contentPane.add(lblUsernameMail);
 
 		JLabel lblEmail_1 = DefaultComponentFactory.getInstance().createLabel("E-mail: ");
-		lblEmail_1.setBounds(299, 430, 68, 14);
+		lblEmail_1.setBounds(316, 430, 41, 17);
 		contentPane.add(lblEmail_1);
 
 		JLabel lblPassword_1 = DefaultComponentFactory.getInstance().createLabel("Password:");
-		lblPassword_1.setBounds(285, 461, 82, 14);
+		lblPassword_1.setBounds(299, 463, 71, 14);
 		contentPane.add(lblPassword_1);
 
 		JButton btnAdicionarFace = new JButton("Adicionar"); // Botão Adiciona tokens - facebook
@@ -244,11 +233,11 @@ public class AccountManageWindow extends JFrame {
 				SwingUtilities.invokeLater(new Runnable() {
 					public void run() {
 						try {
-							if(!textField_1.getText().equals(null) 
+							if(!authConsumKeyTwit.getText().equals(null) 
 									&& !textField_10.getText().equals(null) 
 									&& !textField_9.getText().equals(null) 
 									&& !textField_3.getText().equals(null)){
-								editor.AddAcountsForTwitter(textField_1.getText(),textField_10.getText(),textField_9.getText(),textField_3.getText());
+								editor.AddAcountsForTwitter(authConsumKeyTwit.getText(),textField_10.getText(),textField_9.getText(),textField_3.getText());
 							}
 							
 						} catch (Exception e) {
@@ -280,8 +269,8 @@ public class AccountManageWindow extends JFrame {
 					public void run() {
 						try {
 							if(!textField_4.getText().equals(null) 
-									&& !textField_8.getText().equals(null)){
-								editor.AddAcountsForEmail(textField_4.getText(), textField_8.getText());
+									&& !passMail.getText().equals(null)){
+								editor.AddAcountsForEmail(textField_4.getText(), passMail.getText());
 							}
 							
 						} catch (Exception e) {
@@ -317,7 +306,7 @@ public class AccountManageWindow extends JFrame {
 			public void mouseClicked(MouseEvent e) {
 
 				if (textField_10.getText().toString().equals("") 
-						|| textField_1.getText().toString().equals("")
+						|| authConsumKeyTwit.getText().toString().equals("")
 						|| textField_9.getText().toString().equals("") 
 						|| textField_3.getText().toString().equals("")) {
 					System.out.println("1 caixa texto=" + textField_10.getText().toString()); // não esta a captar info
@@ -328,7 +317,7 @@ public class AccountManageWindow extends JFrame {
 					XMLFileEditor i = new XMLFileEditor();
 					System.out.println("chego aqui");
 
-					String s = i.addTwittwerTokens(textField_10.getText().toString(), textField_1.getText().toString(),
+					String s = i.addTwittwerTokens(textField_10.getText().toString(), authConsumKeyTwit.getText().toString(),
 							textField_9.getText().toString(), textField_3.getText().toString());
 
 					if (s.equals("Error")) {
@@ -340,7 +329,7 @@ public class AccountManageWindow extends JFrame {
 						System.out.println(textField_10.getText().toString());
 						System.out.println(textField_9.getText().toString());
 						System.out.println(textField_3.getText().toString());
-						System.out.println(textField_1.getText().toString());
+						System.out.println(authConsumKeyTwit.getText().toString());
 					} else {
 						infoLabel.setText(
 								"<html><font color='green'> Twitter Tokens successfully registered!</font></html>");
@@ -357,16 +346,16 @@ public class AccountManageWindow extends JFrame {
 			public void mouseClicked(MouseEvent e) {
 				
 				try {
-				if (textField_11.getText().equals("") 
+				if (usernameMail.getText().equals("") 
 						|| textField_4.getText().equals("")
-						|| textField_8.getText().equals("")) {
+						|| passMail.getText().equals("")) {
 					System.out.println("1 caixa texto=" + textField_4.getText()); // não esta a captar info da caixa, problema?
 					infoLabel.setText("<html><font color='red'>Error: All input fields are mandatory. Insert all e-mail inputs! </font></html>");
 
 				} else {
 					XMLFileEditor i = new XMLFileEditor();
-					String s = i.SignIn(textField_11.getText().toString(), textField_4.getText().toString(),
-							textField_8.getText().toString());
+					String s = i.SignIn(usernameMail.getText().toString(), textField_4.getText().toString(),
+							passMail.getText().toString());
 
 					if (s.equals("Error")) {
 						infoLabel.setText("<html><font color='red'>Error</font></html>");
